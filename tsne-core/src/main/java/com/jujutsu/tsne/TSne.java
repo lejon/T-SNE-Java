43ec0f0cce5a0bd9af1ea05f8086f3191c35158f
Leif Jonsson <leif.jonsson@ericsson.com>
2016-11-02 08:03:41 +0100

    Removed hbeta and x2p from tsne api.

--- tsne-core/src/main/java/com/jujutsu/tsne/TSne.java
+++ tsne-core/src/main/java/com/jujutsu/tsne/TSne.java
@@ -16,10 +16,6 @@ public interface TSne {
 
 	double [][] tsne(double[][] X, int no_dims, int initial_dims, double perplexity, int max_iter, boolean use_pca);
 
-	R Hbeta (double [][] D, double beta);
-	
-	R x2p(double [][] X,double tol, double perplexity);
-
 	static class R {
 		double [][] P;
 		double [] beta;
